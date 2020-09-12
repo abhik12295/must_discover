3e2d8340a1268efe9ea8194f8ab2d5cc4764f1a1
Abhishek Kumar <abhik12295@gmail.com>
2020-09-12 20:42:25 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-	MongoURI: 'mongodb+srv://abhi_1294:Gmail@123@must-discover.btiam.mongodb.net/<dbname>?retryWrites=true&w=majority'
+	MongoURI: 'mongodb+srv://username:password@must-discover.btiam.mongodb.net/<dbname>?retryWrites=true&w=majority'
 }
